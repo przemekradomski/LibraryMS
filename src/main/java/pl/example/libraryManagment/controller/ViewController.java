23df0674acb19ae1ea98ac9f5b3fb887bc8bd88d
przemekradomski <przemekradomski@protonmail.com>
2026-08-15 16:06:30 +0200

    Naprawiono widok Statystyk na stronie głównej

--- src/main/java/pl/example/libraryManagment/controller/ViewController.java
+++ src/main/java/pl/example/libraryManagment/controller/ViewController.java
@@ -5,22 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.example.libraryManagment.service.BookService;
+import pl.example.libraryManagment.service.MemberService;
+import pl.example.libraryManagment.service.ReservationService;
 
 @Controller
 public class ViewController {
 
     private final BookService bookService;
+    private final MemberService memberService;
+    private final ReservationService reservationService;
 
-    public ViewController(BookService bookService) {
+    public ViewController(BookService bookService, MemberService memberService, ReservationService reservationService) {
         this.bookService = bookService;
+        this.memberService = memberService;
+        this.reservationService = reservationService;
     }
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("numberOfBooks", bookService.getAllBooks().size());
-        model.addAttribute("numberOfActiveMembers", 0);
-        model.addAttribute("numberOfBorrowedBooks", 0);
-        model.addAttribute("numberOfReservation", 0);
+        model.addAttribute("numberOfActiveMembers", memberService.getAllMembers().size());
+        model.addAttribute("numberOfReservation", reservationService.getAllReservations().size());
         model.addAttribute("lastAddedBooks", bookService.GetLastAddedBooks());
         model.addAttribute("lastReservations", List.of());
         return "index";
